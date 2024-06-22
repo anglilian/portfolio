c990b0deb3affb7f477bb26fb07d14d41072148d
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-06-22 15:47:07 +0100

    Changed typing text to be variable

--- my-portfolio-v2/src/app/page.tsx
+++ my-portfolio-v2/src/app/page.tsx
@@ -2,5 +2,9 @@ import TypingText from "../components/TypingText";
 import React from "react";
 
 export default function Home() {
-  return <TypingText />;
+  return (
+    <div className="w-full max-w-4xl min-h-screen items-center flex flex-col py-8">
+      <TypingText text="Hello, Li-Lian here!"></TypingText>
+    </div>
+  );
 }
--- my-portfolio-v2/src/components/TypingText.tsx
+++ my-portfolio-v2/src/components/TypingText.tsx
@@ -1,70 +1,37 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from "react";
-import "../styles/TypingText.css"; // Import the CSS file
+import React, { useEffect, useState } from "react";
+import "../styles/TypingText.css"; // Ensure the CSS file is imported
 
-const TypingText = () => {
-  const carouselText = [
-    { text: "A Digital Artist", color: "#000090", tag: "digital-art" },
-    { text: "An Educator", color: "#1D00FF", tag: "education" },
-    { text: "A Writer", color: "#8D04FB", tag: "writing" },
-    { text: "An Event Organiser", color: "#FF4EB1", tag: "events" },
-    { text: "A Coder", color: "#FF9669", tag: "coding" },
-  ];
+interface TypingTextProps {
+  text: string;
+}
 
+const TypingText: React.FC<TypingTextProps> = ({ text }) => {
   const [displayedText, setDisplayedText] = useState("");
   const [index, setIndex] = useState(0);
-  const [textIndex, setTextIndex] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [waiting, setWaiting] = useState(false);
 
   const getRandomTypingSpeed = () => {
     return Math.random() * (200 - 100) + 100; // Random delay between 100ms and 200ms
   };
 
   useEffect(() => {
-    let timeout;
+    let timeout: string | number | NodeJS.Timeout | undefined;
 
-    if (waiting) {
+    if (index < text.length) {
       timeout = setTimeout(() => {
-        setWaiting(false);
-        setIsDeleting(true);
-      }, 2000); // Pause before deleting
-    } else if (isDeleting) {
-      if (index > 0) {
-        timeout = setTimeout(() => {
-          setDisplayedText((prev) => prev.slice(0, -1));
-          setIndex(index - 1);
-        }, 100); // Adjust deleting speed here
-      } else {
-        setIsDeleting(false);
-        setTextIndex((prev) => (prev + 1) % carouselText.length);
-        timeout = setTimeout(() => setIndex(1), 1000); // Pause before typing again
-      }
-    } else {
-      if (index <= carouselText[textIndex].text.length) {
-        timeout = setTimeout(() => {
-          setDisplayedText(carouselText[textIndex].text.slice(0, index));
-          setIndex(index + 1);
-        }, getRandomTypingSpeed()); // Use random typing speed
-      } else {
-        setWaiting(true);
-        timeout = setTimeout(() => {}, 2000); // Pause before deleting (this timeout does nothing, just for clarity)
-      }
+        setDisplayedText((prev) => prev + text[index]);
+        setIndex(index + 1);
+      }, getRandomTypingSpeed()); // Use random typing speed
     }
 
     return () => clearTimeout(timeout);
-  }, [carouselText, index, textIndex, isDeleting, waiting]);
+  }, [text, index]);
 
   return (
-    <div className="typing-container">
-      <div className="text-cursor-container">
-        <div className="mr-2">I AM </div>
-        <div style={{ color: carouselText[textIndex].color }}>
-          {displayedText}
-        </div>
-        <span className="input-cursor"></span>
-      </div>
+    <div className="flex items-baseline">
+      <div className="text-xxl">{displayedText}</div>
+      <span className="input-cursor"></span>
     </div>
   );
 };
