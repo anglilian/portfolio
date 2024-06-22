7e958a7771ddc9cdefa19eb487cc9130704f4eea
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-06-22 11:01:28 +0100

    Add nav bar to new portfolio

---
+++ my-portfolio/app/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return <p>Hello</p>;
+}
---
+++ my-portfolio/components/Navbar.tsx
@@ -0,0 +1,46 @@
+// src/components/Navbar.js
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/images/logo-03.png"; // Adjust the path as needed
+
+const Navbar = () => {
+  return (
+    <nav className="shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center max-h-10">
+        <Link href="/" passHref>
+          <div className="flex items-center h-full">
+            <div className="relative h-full w-auto">
+              <Image
+                src={logo}
+                alt="Logo"
+                layout="intrinsic"
+                objectFit="contain"
+                className="max-h-10 w-auto"
+              />
+            </div>
+          </div>
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="#home" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="#about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="#projects" className="nav-link">
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
