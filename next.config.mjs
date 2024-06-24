404a4ae4c7acd5b3ba280b9beed2fe7a8e189d3e
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-06-25 00:05:20 +0100

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-};
+export default nextConfig;
