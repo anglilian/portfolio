4b6c02cb032cabcccefebc5ddb529ee5f1f05f97
Ang Li-Lian <anglilian@uni.minerva.edu>
2024-09-02 22:14:46 +0100

    Fix images in blog not showing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enable React Strict Mode for better development practices
+
+  images: {
+    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"], // Replace with your actual domain for images from Notion or other sources
+  },
+
+  // You can add more configurations here as needed
+};
 
 export default nextConfig;
