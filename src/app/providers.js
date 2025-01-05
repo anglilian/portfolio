d7fd253515021b0acc82e6249d00628a17248190
Ang Li-Lian <anglilian@uni.minerva.edu>
2025-01-05 19:52:36 +0800

    add posthog tracking

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,19 +1,14 @@
+// app/providers.js
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
-import { ReactNode } from "react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
-
-interface CSPostHogProviderProps {
-  children: ReactNode;
-}
-
-export function CSPostHogProvider({ children }: CSPostHogProviderProps) {
+export function CSPostHogProvider({ children }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
